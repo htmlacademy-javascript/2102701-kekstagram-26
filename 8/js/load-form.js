65e3237ea99e3d349849f00afe29c29a40c21e95
Keks <keks@htmlacademy.ru>
2022-07-04 17:58:31 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/load-form.js
+++ 8/js/load-form.js
@@ -1,55 +1,72 @@
+import {initModal} from './modal.js';
+
 const imgUploadForm = document.querySelector('#upload-select-image');
 imgUploadForm.setAttribute('action', 'https://26.javascript.pages.academy/kekstagram');
 const uploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
-const buttonCancel = imgUploadForm.querySelector('#upload-cancel');
 const inputFile = imgUploadForm.querySelector('#upload-file');
-const pristine = new Pristine(imgUploadForm);
-const buttonSubmit = imgUploadForm.querySelector('.img-upload__submit');
+const pristine = new Pristine(imgUploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper'
+});
 const inputHT = imgUploadForm.querySelector('.text__hashtags');
 const textComment = document.querySelector('.text__description');
 
-const regexp = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}/;
+const regexp = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 
-const uplOpen = function () {
-  uploadOverlay.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-};
-
-const uplClose = function () {
-  uploadOverlay.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  inputFile.value = '';
-  inputHT.textContent = '';
-  textComment.textContent = '';
-};
-
-inputFile.addEventListener('change', (evt) => {
-  uplOpen();
+const {open} = initModal(uploadOverlay, {
+  onClose:()=>{inputFile.value = '';
+    inputHT.textContent = '';
+    textComment.textContent = '';
+  }
 });
 
-buttonCancel.addEventListener('click', uplClose);
+inputFile.addEventListener('change', open);
+
+const validateHashTagMessages = [];
 
 const validateHashTags = function (value) {
-  return regexp;
+  validateHashTagMessages.length=0;
+  const tags = value.split(' ');
+  if (tags.length > 5) {
+    validateHashTagMessages.push('Тегов слишком много');
+    return false;
+  }
+  const usedTags=[];
+
+  tags.forEach((value) => {
+    if (!regexp.test(value)) {
+      validateHashTagMessages.push(`таг ${value} содердит недопустимые символы или длина больше 20 символов`);
+    }
+    if (usedTags.includes(value.toLowerCase())) {
+      validateHashTagMessages.push(`тэг ${value} повторяется`);
+    } else {
+      usedTags.push(value.toLowerCase());
+    }
+  });
+  return !validateHashTagMessages.length;
+};
+
+const showValidatedHashTagsMessages = function () {
+  return validateHashTagMessages.join(', ');
 };
 
 const validateComments = function (value) {
   return value.length <= 140;
 };
-pristine.addValidator(inputHT, validateHashTags);
-pristine.addValidator(textComment, validateComments);
+pristine.addValidator(inputHT, validateHashTags, showValidatedHashTagsMessages);
+pristine.addValidator(textComment, validateComments, 'gnxdfbdrsb');
 
-buttonSubmit.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-});
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.keyCode === 27) {
-    if (inputHT === document.activeElement || textComment === document.activeElement) {
-      return evt;
-    } else {
-      uplClose();
-    }
+imgUploadForm.addEventListener('submit', (evt) => {
+  if (!pristine.validate()){
+    evt.preventDefault();
   }
 });
+
+const escStopPropagnation= function (evt) {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+};
+
+inputHT.addEventListener('keydown', escStopPropagnation);
+textComment.addEventListener('keydown', escStopPropagnation);
